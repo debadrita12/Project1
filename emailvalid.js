ab4a2c9f878adbc52b8c1f84482c5862278efe78
unknown <debadrita>
2020-10-10 21:21:51 +0530

    initial commit

---
+++ emailvalid.js
@@ -0,0 +1,14 @@
+function ValidateEmail()
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+
+}
+}
